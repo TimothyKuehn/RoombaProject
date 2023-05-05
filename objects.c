330cedabb00a0cc3128215a13bfe3ae75dbeefcb
MattWells6 <131848209+MattWells6@users.noreply.github.com>
2023-05-05 14:34:57 -0500

    Update objects.c

--- objects.c
+++ objects.c
@@ -1,9 +1,11 @@
-/*
+/**
+ * 
  * objects.c
- *
- *  Created on: Feb 18, 2023
- *      Author: mdwells
- */
+ * 
+ * Created Feb 18, 2023
+ *    @author mdwells
+ * 
+ **/
 
 #include <stdio.h>
 #include <stdint.h>
@@ -19,16 +21,18 @@
 #include "Scanner.h"
 #include "ping.h"
 
+// Structure for representing detected objects
 typedef struct
 {
-    float distance;
-    int angle;
-    float width;
+float distance;
+int angle;
+float width;
 } Objects;
 
+// Function for sending the array of detected objects over UART
 void sendObjectArr(Objects *arr, int numObjects)
 {
-
+    // Create a string array
     char objects[10][50];
     int i;
     for (i = 0; i < numObjects; ++i)
@@ -36,36 +40,38 @@ void sendObjectArr(Objects *arr, int numObjects)
         float dist = arr[i].distance;
         int angle = arr[i].angle;
         float rad = arr[i].width;
-        sprintf(objects[i], "%d!%.2f!%.2f", angle, dist, rad);
+        // Create a string for the object and put into the sting array
+        sprintf(objects[i], "%d!%.2f!%.2f", angle, dist, rad); 
     }
 
-    char st[200] = "I";
+
+    char st[200] = "I"; // Trigger identifier "I"
     for (i = 0; i < numObjects; ++i)
     {
-       // int j;
-     //   for(j = 0; j< strlen(objects[i]); ++j){
-            char object[50];
-            strcpy(object, objects[i]);
-            strcat(st, object);
+        char object[50];
+        strcpy(object, objects[i]); // Pull a single object from the string array
+        strcat(st, object); // Concatenate the object onto st
 
+        // Checks if there is another object to add
         if (numObjects - i > 1)
         {
-            char end[] = "@";
-            strcat(st, end);
+            char end[] = "@"; 
+            strcat(st, end); // Concatenate an indicator for a new object in st
         }
     }
 
     printf(st);
     uart_sendStr(st);
-
 }
 
-
+// function for identifying objects in the scanner's field of view
 void identifyObjects(float inputDistances[])
 {
-    flag_s = false;
+    flag_s = false; // Reset the flag
     int j;
     float distances[180];
+    
+    // Invert and scale the IR values
     for (j = 0; j < 180; j++)
     {
         distances[j] = 100000 / (*(inputDistances + j));
@@ -79,9 +85,10 @@ void identifyObjects(float inputDistances[])
     bool inObj = false;
     for (i = 0; i < 179; ++i)
     {
-
         //detect first edge
-        if ((distances[i] - distances[i + 1] > 20) && (!inObj) && distances[i + 1]) // may need to change the 90 once on the test field
+        if ((distances[i] - distances[i + 1] > 20) && // If the next angle has a signifigantly lower distance
+            (!inObj) && // If not currently in an object
+            distances[i + 1]) // If there is a next angle
         {
             start = i;
             inObj = true;
@@ -92,17 +99,15 @@ void identifyObjects(float inputDistances[])
         {
             if ((distances[i + 1] - distances[i] > 10) && (inObj))
             {
-
                 end = i;
                 if ((end - start) > 8)
                 {
-
                     //calculate average angle of object
                     int angle = (start + end) / 2;
                     objectArr[numObjects].angle = angle;
 
                     //distance of object
-                    float objectDistance = scannerPING(angle); //fails here sometime, check after sendObjectArr is fixed.
+                    float objectDistance = scannerPING(angle);
                     objectArr[numObjects].distance = objectDistance;
 
                     //radial width
@@ -110,17 +115,22 @@ void identifyObjects(float inputDistances[])
                     objectArr[numObjects].width = width;
                     numObjects = numObjects + 1;
                 }
+            
+                // Reset object boolean and start angle
                 inObj = false;
                 start = 0;
             }
         }
     }
-
+    
+    // Send the object array to the GUI
     if (numObjects > 0)
     {
-        sendObjectArr(objectArr,numObjects);
+       sendObjectArr(objectArr,numObjects);
     }
+    
+    // Send an indicator that no objects were found
     if (numObjects < 1){
-        uart_sendStr("K");
+       uart_sendStr("K");
     }
 }
