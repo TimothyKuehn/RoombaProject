381c62f9b96b38358de8241613d70213c609e708
jtyost <131737711+jtyost@users.noreply.github.com>
2023-05-05 20:31:43 +0000

    Add files via upload

--- movement.c
+++ movement.c
@@ -1,9 +1,9 @@
-/**
+/*
  * movement.c
- * Created on: Feb 3, 2023
- *    Author: mdwells
- * 
-**/
+ *
+ *  Created on: Feb 3, 2023
+ *      Author: mdwells
+ */
 
 #include "open_interface.h"
 #include "movement.h"
@@ -24,48 +24,90 @@ double move_forward(oi_t *sensor_data, double distance_mm)
     double sum = 0;
     int speed = 100;
     oi_setWheels(speed, speed); // move forward slowly
-
-    while (abs(sum) < abs(distance_mm)) { // accumulate distance
+	
+    while (sum < distance_mm)
+    {
         oi_update(sensor_data);
-        sum += (speed < 0 ? -1 : 1) * sensor_data->distance; // update distance based on direction
-        
-        // stop and return if any sensors detect an obstacle
-        if (sensor_data->cliffFrontLeft || sensor_data->cliffFrontRight ||
-            sensor_data->bumpRight || sensor_data->bumpLeft ||
-            sensor_data->cliffLeft || sensor_data->cliffRight ||
-            sensor_data->cliffFrontLeftSignal > 2700 ||
-            sensor_data->cliffFrontRightSignal > 2700 ||
-            sensor_data->cliffLeftSignal > 2700 ||
-            sensor_data->cliffRightSignal > 2700) {
+        sum += sensor_data->distance; //accumulate distance
+		
+		//if statements to stop the bot and send the GUI information if any of the cliff/bump sensors are triggered
+        if (sensor_data->cliffFrontLeft)
+        {
             stop();
-            if (sensor_data->cliffFrontLeft) {
-                sprintf(string, "C1");
-            } else if (sensor_data->cliffFrontRight) {
-                sprintf(string, "C2");
-            } else if (sensor_data->bumpRight) {
-                sprintf(string, "B1");
-            } else if (sensor_data->bumpLeft) {
-                sprintf(string, "B0");
-            } else if (sensor_data->cliffLeft) {
-                sprintf(string, "C0");
-            } else if (sensor_data->cliffRight) {
-                sprintf(string, "C3");
-            } else if (sensor_data->cliffFrontLeftSignal > 2700) {
-                sprintf(string, "L1");
-            } else if (sensor_data->cliffFrontRightSignal > 2700) {
-                sprintf(string, "L2");
-            } else if (sensor_data->cliffLeftSignal > 2700) {
-                sprintf(string, "L0");
-            } else if (sensor_data->cliffRightSignal > 2700) {
-                sprintf(string, "L3");
-            }
+            sprintf(string, "C1"); //indicates to the GUI which sensor was trigger
+            uart_sendStr(string); 
+            return sum;
+        }
+        if (sensor_data->cliffFrontRight)
+        {
+            stop();
+            sprintf(string, "C2");
             uart_sendStr(string);
-            return abs(sum);
+            return sum;
+        }
+        if (sensor_data->bumpRight)
+        {
+            stop();
+            sprintf(string, "B1");
+            uart_sendStr(string);
+            return sum;
+        }
+        if (sensor_data->bumpLeft)
+        {
+            stop();
+            sprintf(string, "B0");
+            uart_sendStr(string);
+            return sum;
+        }
+        if (sensor_data->cliffLeft)
+        {
+            stop();
+            sprintf(string, "C0");
+            uart_sendStr(string);
+            return sum;
+        }
+
+        if (sensor_data->cliffRight)
+        {
+            stop();
+            sprintf(string, "C3");
+            uart_sendStr(string);
+            return sum;
+        }
+        if (sensor_data->cliffFrontLeftSignal > 2700)
+        {
+            stop();
+            sprintf(string, "L1");
+            uart_sendStr(string);
+            return sum;
+        }
+        if (sensor_data->cliffFrontRightSignal > 2700)
+        {
+            stop();
+            sprintf(string, "L2");
+            uart_sendStr(string);
+            return sum;
+        }
+        if (sensor_data->cliffLeftSignal > 2700)
+        {
+            stop();
+            sprintf(string, "L0");
+            uart_sendStr(string);
+            return sum;
+        }
+
+        if (sensor_data->cliffRightSignal > 2700)
+        {
+            stop();
+            sprintf(string, "L3");
+            uart_sendStr(string);
+            return sum;
         }
     }
 
-    oi_setWheels(0, 0); // stop
-    return abs(sum);
+    oi_setWheels(0, 0); //stop
+
+    return sum;
 }
 
 //Moves the bot some distance
@@ -83,8 +125,7 @@ double move_backward(oi_t *sensor_data, double distance_mm)
     return (-1 * sum);
 }
 
-
-//stops the bot and backs up to scan
+//stops the bot
 void stop()
 {
     oi_setWheels(0, 0); //stop
@@ -93,28 +134,30 @@ void stop()
 }
 
 // Turns the bot right some degrees
-double turn_right(oi_t *sensor, double degrees) 
+double turn_right(oi_t *sensor, double degrees)
 {
-    return turn(sensor, degrees, -50, 50);
+    oi_setWheels(-50, 50);
+    double sum = 0;
+    while (sum < degrees)
+    {
+        oi_update(sensor);
+        sum -= sensor->angle; // use -> notation since pointer
+    }
+    oi_setWheels(0, 0); //stop
+    return sum;
 }
 
 // Turns the bot left some degrees
-double turn_left(oi_t *sensor, double degrees) 
+double turn_left(oi_t *sensor, double degrees)
 {
-    return turn(sensor, degrees, 50, -50);
-}
-
-// Abstact turning function
-double turn(oi_t *sensor, double degrees, int left_speed, int right_speed) 
-{
+    oi_setWheels(50, -50);
     double sum = 0;
-    oi_setWheels(left_speed, right_speed); // set the wheels to turn
-
-    while (abs(sum) < abs(degrees)) { // accumulate angle
+    while (sum < degrees)
+    {
         oi_update(sensor);
-        sum += (left_speed < 0 ? -1 : 1) * sensor->angle; // update angle based on direction
+        sum += sensor->angle; // use -> notation since pointer
     }
-
-    oi_setWheels(0, 0); // stop
-    return abs(sum);
+    oi_setWheels(0, 0); //stop
+    return -1 * sum;
 }
+
