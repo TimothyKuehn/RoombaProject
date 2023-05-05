a29f1465c5c0d18c80da851f79d173fdb46d4678
MattWells6 <131848209+MattWells6@users.noreply.github.com>
2023-05-05 14:57:03 -0500

    Update movement.c

--- movement.c
+++ movement.c
@@ -1,9 +1,9 @@
-/*
+/**
  * movement.c
- *
- *  Created on: Feb 3, 2023
- *      Author: mdwells
- */
+ * Created on: Feb 3, 2023
+ *    Author: mdwells
+ * 
+**/
 
 #include "open_interface.h"
 #include "movement.h"
@@ -17,93 +17,55 @@
 #include <inc/tm4c123gh6pm.h>
 #include <uart-interrupt.h>
 
-//Moves the bot some distance
+// Moves the bot forward a set distance
 double move_forward(oi_t *sensor_data, double distance_mm)
 {
     char string[3];
     double sum = 0;
-    oi_setWheels(100, 100); //move forward slowly
-    while (sum < distance_mm)
-    {
+    int speed = 100;
+    oi_setWheels(speed, speed); // move forward slowly
+
+    while (abs(sum) < abs(distance_mm)) { // accumulate distance
         oi_update(sensor_data);
-        sum += sensor_data->distance; //accumulate distance
-        if (sensor_data->cliffFrontLeft)
-        {
+        sum += (speed < 0 ? -1 : 1) * sensor_data->distance; // update distance based on direction
+        
+        // stop and return if any sensors detect an obstacle
+        if (sensor_data->cliffFrontLeft || sensor_data->cliffFrontRight ||
+            sensor_data->bumpRight || sensor_data->bumpLeft ||
+            sensor_data->cliffLeft || sensor_data->cliffRight ||
+            sensor_data->cliffFrontLeftSignal > 2700 ||
+            sensor_data->cliffFrontRightSignal > 2700 ||
+            sensor_data->cliffLeftSignal > 2700 ||
+            sensor_data->cliffRightSignal > 2700) {
             stop();
-            sprintf(string, "C1");
-            uart_sendStr(string); //Sends GUI location of Cliff Sensor triggered
-            return sum;
-        }
-        if (sensor_data->cliffFrontRight)
-        {
-            stop();
-            sprintf(string, "C2");
+            if (sensor_data->cliffFrontLeft) {
+                sprintf(string, "C1");
+            } else if (sensor_data->cliffFrontRight) {
+                sprintf(string, "C2");
+            } else if (sensor_data->bumpRight) {
+                sprintf(string, "B1");
+            } else if (sensor_data->bumpLeft) {
+                sprintf(string, "B0");
+            } else if (sensor_data->cliffLeft) {
+                sprintf(string, "C0");
+            } else if (sensor_data->cliffRight) {
+                sprintf(string, "C3");
+            } else if (sensor_data->cliffFrontLeftSignal > 2700) {
+                sprintf(string, "L1");
+            } else if (sensor_data->cliffFrontRightSignal > 2700) {
+                sprintf(string, "L2");
+            } else if (sensor_data->cliffLeftSignal > 2700) {
+                sprintf(string, "L0");
+            } else if (sensor_data->cliffRightSignal > 2700) {
+                sprintf(string, "L3");
+            }
             uart_sendStr(string);
-            return sum;
-        }
-        if (sensor_data->bumpRight)
-        {
-            stop();
-            sprintf(string, "B1");
-            uart_sendStr(string);
-            return sum;
-        }
-        if (sensor_data->bumpLeft)
-        {
-            stop();
-            sprintf(string, "B0");
-            uart_sendStr(string);
-            return sum;
-        }
-        if (sensor_data->cliffLeft)
-        {
-            stop();
-            sprintf(string, "C0");
-            uart_sendStr(string);
-            return sum;
-        }
-
-        if (sensor_data->cliffRight)
-        {
-            stop();
-            sprintf(string, "C3");
-            uart_sendStr(string);
-            return sum;
-        }
-        if (sensor_data->cliffFrontLeftSignal > 2700)
-        {
-            stop();
-            sprintf(string, "L1");
-            uart_sendStr(string);
-            return sum;
-        }
-        if (sensor_data->cliffFrontRightSignal > 2700)
-        {
-            stop();
-            sprintf(string, "L2");
-            uart_sendStr(string);
-            return sum;
-        }
-        if (sensor_data->cliffLeftSignal > 2700)
-        {
-            stop();
-            sprintf(string, "L0");
-            uart_sendStr(string);
-            return sum;
-        }
-
-        if (sensor_data->cliffRightSignal > 2700)
-        {
-            stop();
-            sprintf(string, "L3");
-            uart_sendStr(string);
-            return sum;
+            return abs(sum);
         }
     }
 
-    oi_setWheels(0, 0); //stop
-
-    return sum;
+    oi_setWheels(0, 0); // stop
+    return abs(sum);
 }
 
 //Moves the bot some distance
@@ -121,7 +83,8 @@ double move_backward(oi_t *sensor_data, double distance_mm)
     return (-1 * sum);
 }
 
-//stops the bot
+
+//stops the bot and backs up to scan
 void stop()
 {
     oi_setWheels(0, 0); //stop
@@ -129,29 +92,29 @@ void stop()
 
 }
 
-double turn_right(oi_t *sensor, double degrees)
+// Turns the bot right some degrees
+double turn_right(oi_t *sensor, double degrees) 
 {
-    oi_setWheels(-50, 50);
-    double sum = 0;
-    while (sum < degrees)
-    {
-        oi_update(sensor);
-        sum -= sensor->angle; // use -> notation since pointer
-    }
-    oi_setWheels(0, 0); //stop
-    return sum;
+    return turn(sensor, degrees, -50, 50);
 }
 
-double turn_left(oi_t *sensor, double degrees)
+// Turns the bot left some degrees
+double turn_left(oi_t *sensor, double degrees) 
 {
-    oi_setWheels(50, -50);
-    double sum = 0;
-    while (sum < degrees)
-    {
-        oi_update(sensor);
-        sum += sensor->angle; // use -> notation since pointer
-    }
-    oi_setWheels(0, 0); //stop
-    return -1 * sum;
+    return turn(sensor, degrees, 50, -50);
 }
 
+// Abstact turning function
+double turn(oi_t *sensor, double degrees, int left_speed, int right_speed) 
+{
+    double sum = 0;
+    oi_setWheels(left_speed, right_speed); // set the wheels to turn
+
+    while (abs(sum) < abs(degrees)) { // accumulate angle
+        oi_update(sensor);
+        sum += (left_speed < 0 ? -1 : 1) * sensor->angle; // update angle based on direction
+    }
+
+    oi_setWheels(0, 0); // stop
+    return abs(sum);
+}
